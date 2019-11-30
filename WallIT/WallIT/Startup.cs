bc9d3a5dbe2647ec4310038796e1a3db1b02b1ea
Sipos Bence <sipos.bnc@gmail.com>
2019-11-30 21:44:15 +0100

    dataAcess reference deleted from web

--- WallIT/WallIT/Startup.cs
+++ WallIT/WallIT/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using WallIT.DataAccess.SessionBuilder;
+using System;
 using WallIT.Shared.Mapping;
 using WallIT.Web.Infrastructure;
 
