e98dfc9f99580b9ae35f1aa7aaad5da12d7f0695
Sipos Bence <sipos.bnc@gmail.com>
2019-12-16 22:40:07 +0100

    localization fix, missing method

--- WallIT/WallIT.Web/Controllers/HomeController.cs
+++ WallIT/WallIT.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
 using System.Diagnostics;
 using WallIT.Web.Models;
 
@@ -6,8 +10,16 @@ namespace WallIT.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
+
         public IActionResult Index()
         {
+            ViewData["Welcome"] = _localizer["Welcome"];
             return View();
         }
 
@@ -20,6 +32,22 @@ namespace WallIT.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SetLanguage(string culture, string returnUrl = "~/")
+        {
+            culture = culture ?? "en-US";
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                }
+            );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
